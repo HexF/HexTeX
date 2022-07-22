f65431662feb9b3aa73b87422c2e08d2fe37af23
Thomas Hobson <thomas@hexf.me>
2022-07-22 14:51:20 +1200

    Render image once and upload to discord

--- commands-tex.sh
+++ commands-tex.sh
@@ -2,10 +2,71 @@
 
 # We have a call!
 
-LATEX=$(jq '.data.options[] | select(.name=="latex").value' "$1" -r | base64 -w0)
-PUBLIC_URL=$(dirname "$REQUEST_SCHEME://$SERVER_NAME$REQUEST_URI")
+BOUNDARY="boundary"
+TEMPDIR=$(mktemp -d)
+TEXFILE=$TEMPDIR/file.tex
 
+echo "Content-Type: multipart/form-data; boundary=--$BOUNDARY"
 echo "Status: 200 OK"
 echo
 
-echo '{"type": 4, "data":{"embeds":[{"image":{"url":"'"$PUBLIC_URL/tex-eqtopng.sh?$LATEX"'"}}]}}'
+
+
+
+cat <<END > $TEXFILE
+\documentclass[border=2pt]{standalone}
+\usepackage{amsmath}
+\usepackage{amsfonts}
+\usepackage{varwidth}
+\begin{document}
+\begin{varwidth}{\linewidth}
+\[
+END
+
+jq '.data.options[] | select(.name=="latex").value' "$1" -r >> $TEXFILE
+
+cat <<END >> $TEXFILE
+\]
+\end{varwidth}
+\end{document}
+END
+
+pdflatex --output-directory $TEMPDIR $TEXFILE >$TEMPDIR/tex.log 2>$TEMPDIR/tex-err.log && \
+    convert -density 300 -background white -alpha remove -quality 50 -colorspace RGB $TEMPDIR/file.pdf $TEMPDIR/file.png >$TEMPDIR/con.log 2>$TEMPDIR/con-err.log
+
+if [ $? -eq 0 ]; then
+    # Success! 
+    
+    # Send through the JSON
+    echo -e "\r"
+    echo "--$BOUNDARY"
+    echo 'Content-Disposition: form-data; name="payload_json"'
+    echo "Content-Type: application/json"
+    echo
+    echo '{"type": 4, "data":{"embeds":[{"image":{"url":"attachment://tex.png"}}]}}'
+    
+    echo -e "\r"
+    echo "--$BOUNDARY"
+    # Send through the image
+    echo 'Content-Disposition: form-data; name="files[0]"; filename="tex.png"'
+    echo "Content-Type: image/png"
+    echo
+    cat $TEMPDIR/file.png
+    
+    # Done
+    echo -e "\r"
+    echo "--$BOUNDARY--"
+else 
+    echo -e "\r"
+    echo "--$BOUNDARY"
+    echo 'Content-Disposition: form-data; name="payload_json"'
+    echo "Content-Type: application/json"
+    echo
+    echo '{"type": 4, "data":{"embeds":[{"title":"Failed to Render"}]}}'
+    # Done
+    echo -e "\r"
+    echo "--$BOUNDARY--"
+fi
+
+
+rm -rf $TEMPDIR
